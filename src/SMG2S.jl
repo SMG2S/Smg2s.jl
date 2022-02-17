b08c34208a2f5fbbacccfccdb271a3e19b49ad9f
brunowu <xinzhe.wu1990@gmail.com>
2022-02-17 19:09:41 +0100

    add non-symmetric case

--- src/SMG2S.jl
+++ src/SMG2S.jl
@@ -7,7 +7,7 @@ using Random
 export Nilpotent, Nilp
 export Spectrum!
 export initMat!
-export nonherm
+export nonherm, nonsym
 # Write your package code here.
 
 struct Nilpotent{Ti<:Integer}
@@ -16,6 +16,15 @@ struct Nilpotent{Ti<:Integer}
     size::Ti
     degree::Ti
     nilpMat::SparseMatrixCSC
+end
+
+function factorial(num::Tv, depth::Ti) where {Tv <: Real, Ti <: Integer}
+    fact = num
+    for i = 1:depth-1
+        fact *= (num-i)
+    end
+
+    return fact
 end
 
 function Nilp(nbOne::Ti, size::Ti) where {Ti<:Integer}
@@ -74,14 +83,6 @@ function Nilp(vector::AbstractVector, size::Ti) where {Ti<:Integer}
 
 end
 
-function Spectrum!(spec::AbstractVector{Tv}, size::Ti) where {Tv<:Complex, Ti<:Integer}
-    rnd = rand(MersenneTwister(1234), Tv, size)
-
-    for i = 1 : size
-        spec[i] = rnd[i]
-    end
-end
-
 function Spectrum!(spec::AbstractVector{Tv}, f::Function, size::Ti) where {Tv<:Complex, Ti<:Integer}
     for i = 1:size
         spec[i] = Tv(f(i))
@@ -138,39 +139,57 @@ function initMat!(
     end
 end
 
-
-function nonherm(
-    nbOne::Ti,
-    size::Ti,
+function initMat!(
+    matrix::SparseMatrixCSC{Tv, Ti},
     diag_l::Ti,
     diag_u::Ti,
-    spectrum::AbstractVector{Tv},
-    Am::SparseMatrixCSC{Tv, Ti}
-) where {Tv<:Complex, Ti<:Integer}
+    size::Ti;
+    scale::Real = 1.0,
+    shift::Real = 0.0,
+    sparsity::Real = 0.9
+) where {Tv<:Real, Ti<:Integer}
 
-    nilp = Nilp(nbOne, size)
-    for i = 1:size
-        Am[i, i] = spectrum[i]
+    diag_l = abs(diag_l)
+    diag_u = abs(diag_u)
+
+    if diag_u < 2
+        error(
+            "for initialisationg of non-symmetric matrix, please ensure abs(diag_u) >= 2 ",
+            )
     end
 
-    matAop = Am
 
-    fact = 1
-    fact = factorial((2 * nilp.nbOne))
-    Am = fact * Am
-
-    for k = 1 : 2 * nilp.nbOne
-        matAop = matAop * nilp.nilpMat - nilp.nilpMat * matAop
-        fact /= factorial((k + 1))
-        Am += fact * matAop
+    if diag_l < diag_u
+        error(
+            "for initialisationg of matrix, please ensure abs(diag_l) < abs(diag_u) ",
+            )
     end
 
-    fact = factorial((2 * nilp.nbOne))
-    Am = (inv(fact)) * Am
+    if diag_u >= size
+        error(
+            "for initialisationg of matrix, please ensure abs(diag_u) < size ",
+            )
+    end
 
-    return Am
+    cnt::Ti = (diag_l - diag_u + 1) * (2 * size - diag_l - diag_u) / 2
+
+    rnd = rand(MersenneTwister(1234), Float64, cnt)
+
+    for i = 1:cnt
+        if real(rnd[i]) < 1.0 - sparsity
+            rnd[i] = 0.0
+        end
+    end
+
+    idx = 0
+    for i = diag_u + 1:size
+        for j = max(1, i - diag_l) : i - diag_u
+            idx += 1
+            matrix[i, j] = Tv(scale * (rnd[idx]) + shift)
+        end
+    end
+
 end
-
 
 function nonherm(
     nbOne::Ti,
@@ -180,6 +199,7 @@ function nonherm(
     spectrum::AbstractVector{Tv}
 ) where {Tv<:Complex, Ti<:Integer}
 
+    Tv2 = eltype(real(spectrum[1]))
     Am = spzeros(Tv, size, size)
     initMat!(Am, diag_l, diag_u, size)
 
@@ -191,19 +211,228 @@ function nonherm(
     matAop = Am
 
     fact = 1
-    fact = factorial((2 * nilp.nbOne))
+    fact = factorial(Tv2(2 * nilp.nbOne),2 * nilp.nbOne)
     Am = fact * Am
 
     for k = 1 : 2 * nilp.nbOne
         matAop = matAop * nilp.nilpMat - nilp.nilpMat * matAop
-        fact /= factorial((k + 1))
+        fact /= factorial(Tv2(k + 1), k+1)
         Am += fact * matAop
     end
 
-    fact = factorial((2 * nilp.nbOne))
+    fact = factorial(Tv2(2 * nilp.nbOne), 2 * nilp.nbOne)
     Am = (inv(fact)) * Am
 
     return Am
 end
+
+
+function nonherm(
+    nbOne::Ti,
+    size::Ti,
+    diag_l::Ti,
+    diag_u::Ti,
+    spectrum::AbstractVector{Tv},
+    Am::SparseMatrixCSC{Tv, Ti}
+) where {Tv<:Complex, Ti<:Integer}
+
+    Tv2 = eltype(real(spectrum[1]))
+
+    nilp = Nilp(nbOne, size)
+    for i = 1:size
+        Am[i, i] = spectrum[i]
+    end
+
+    matAop = Am
+
+    fact = 1
+    fact = factorial(Tv2(2 * nilp.nbOne),2 * nilp.nbOne)
+    Am = fact * Am
+
+    for k = 1 : 2 * nilp.nbOne
+        matAop = matAop * nilp.nilpMat - nilp.nilpMat * matAop
+        fact /= factorial(Tv2(k + 1),k+1)
+        Am += fact * matAop
+    end
+
+    fact = factorial(Tv2(2 * nilp.nbOne),2 * nilp.nbOne)
+    Am = (inv(fact)) * Am
+
+    return Am
+end
+
+function nonherm(
+    size::Ti,
+    diag_l::Ti,
+    diag_u::Ti,
+    spectrum::AbstractVector{Tv},
+    Am::SparseMatrixCSC{Tv, Ti},
+    nilp::Nilpotent{Ti}
+) where {Tv<:Complex, Ti<:Integer}
+
+    Tv2 = eltype(real(spectrum[1]))
+    nbOne = nilp.nbOne
+    for i = 1:size
+        Am[i, i] = spectrum[i]
+    end
+
+    matAop = Am
+
+    fact = 1
+    fact = factorial(Tv2(2 * nilp.nbOne), 2 * nilp.nbOne)
+    Am = fact * Am
+
+    for k = 1 : 2 * nilp.nbOne
+        matAop = matAop * nilp.nilpMat - nilp.nilpMat * matAop
+        fact /= factorial(Tv2(k + 1),k+1)
+        Am += fact * matAop
+    end
+
+    fact = factorial(Tv2(2 * nilp.nbOne),2 * nilp.nbOne)
+    Am = (inv(fact)) * Am
+
+    return Am
+end
+
+
+function nonsym(
+    nbOne::Ti,
+    size::Ti,
+    diag_l::Ti,
+    diag_u::Ti,
+    spectrum::AbstractVector{Tv}
+) where {Tv<:Complex, Ti<:Integer}
+
+    Tv2 = eltype(real(spectrum[1]))
+
+    Am = spzeros(Tv2, size, size)
+    initMat!(Am, diag_l, diag_u, size)
+
+    nilp = Nilp(nbOne, size)
+
+    for i = 1:size
+        Am[i, i] = real(spectrum[i])
+    end
+
+    idx = 1
+    while idx < size
+        if imag(spectrum[idx]) == 0
+            step = 1
+        else
+            step = 2
+        end
+        Am[idx, idx+1] = abs(imag(spectrum[idx]))
+        Am[idx+1,idx] = -abs(imag(spectrum[idx]))
+        idx += step
+    end
+
+    matAop = Am
+
+    fact = 1
+    fact = factorial(Tv2(2 * nilp.nbOne),2 * nilp.nbOne)
+    Am = fact * Am
+
+    for k = 1 : 2 * nilp.nbOne
+        matAop = matAop * nilp.nilpMat - nilp.nilpMat * matAop
+        fact /= factorial(Tv2(k + 1),k+1)
+        Am += fact * matAop
+    end
+
+    fact = factorial(Tv2(2 * nilp.nbOne),2 * nilp.nbOne)
+    Am = (inv(fact)) * Am
+
+    return Am
+end
+
+
+function nonsym(
+    nbOne::Ti,
+    size::Ti,
+    diag_l::Ti,
+    diag_u::Ti,
+    spectrum::AbstractVector{Tv},
+    Am::SparseMatrixCSC{Tv2, Ti},
+) where {Tv<:Complex, Tv2<:Real, Ti<:Integer}
+
+    nilp = Nilp(nbOne, size)
+
+    for i = 1:size
+        Am[i, i] = real(spectrum[i])
+    end
+
+    idx = 1
+    while idx < size
+        if imag(spectrum[idx]) == 0
+            step = 1
+        else
+            step = 2
+        end
+        Am[idx, idx+1] = abs(imag(spectrum[idx]))
+        Am[idx+1,idx] = -abs(imag(spectrum[idx]))
+        idx += step
+    end
+
+    matAop = Am
+
+    fact = 1
+    fact = factorial(Tv2(2 * nilp.nbOne),2 * nilp.nbOne)
+    Am = fact * Am
+
+    for k = 1 : 2 * nilp.nbOne
+        matAop = matAop * nilp.nilpMat - nilp.nilpMat * matAop
+        fact /= factorial(Tv2(k + 1),k+1)
+        Am += fact * matAop
+    end
+
+    fact = factorial(Tv2(2 * nilp.nbOne),2 * nilp.nbOne)
+    Am = (inv(fact)) * Am
+
+    return Am
+end
+
+
+function nonsym(
+    size::Ti,
+    diag_l::Ti,
+    diag_u::Ti,
+    spectrum::AbstractVector{Tv},
+    Am::SparseMatrixCSC{Tv2, Ti},
+    nilp::Nilpotent{Ti}
+) where {Tv<:Complex, Tv2<:Real, Ti<:Integer}
+
+    for i = 1:size
+        Am[i, i] = real(spectrum[i])
+    end
+
+    idx = 1
+    while idx < size
+        if imag(spectrum[idx]) == 0
+            step = 1
+        else
+            step = 2
+        end
+        Am[idx, idx+1] = abs(imag(spectrum[idx]))
+        Am[idx+1,idx] = -abs(imag(spectrum[idx]))
+        idx += step
+    end
+
+    matAop = Am
+
+    fact = 1
+    fact = factorial(Tv2(2 * nilp.nbOne),(2 * nilp.nbOne))
+    Am = fact * Am
+
+    for k = 1 : 2 * nilp.nbOne
+        matAop = matAop * nilp.nilpMat - nilp.nilpMat * matAop
+        fact /= factorial(Tv2(k + 1),k+1)
+        Am += fact * matAop
+    end
+
+    fact = factorial(Tv2(2 * nilp.nbOne), 2 * nilp.nbOne)
+    Am = (inv(fact)) * Am
+
+    return Am
+end
+
 
 end #endmodule
--- test/runtests.jl
+++ test/runtests.jl
@@ -6,24 +6,100 @@ using Plots
 using LinearAlgebra
 using Random
 
-size = 1000
-diag_l = -10
-diag_u = -2
-nbOne = 8
+size = 2000
+diag_l = -120
+diag_u = -5
+nbOne = 40
 
-function f(idx::Integer, size = size)
+nilpvec = zeros(Int64, size)
+
+cnt = 0
+
+while cnt <= size
+    l₁ = rand(1:nbOne)
+    l₀ = rand(1:nbOne)
+
+    for i = cnt+1:l₁+cnt
+        if i <= size
+            nilpvec[i] = 1
+        end
+    end
+
+    for i = cnt+l₁+1:l₁+l₀+cnt
+        if i <= size
+            nilpvec[i] = 0
+        end
+    end
+
+    global cnt += l₀ + l₁
+
+end
+
+#@info nilpvec
+nilp = Nilp(nilpvec, size)
+
+## No hermitian case
+function f1(idx::Integer, size = size)
     return cos((idx-1) * 2 * π / size) + 1 * rand() + 10 + 10 * sin((idx-1) * 2 * π / size) * im
 end
 
-spec = zeros(ComplexF64, size)
-Spectrum!(spec, f, size)
+spec1 = zeros(ComplexF64, size)
+Spectrum!(spec1, f1, size)
 
-Am = spzeros(ComplexF64, size, size)
-initMat!(Am, diag_l, diag_u, size; scale=0.1, sparsity=0.5)
+Am1 = spzeros(ComplexF64, size, size)
+initMat!(Am1, diag_l, diag_u, size; scale=0.1, sparsity=0.005)
 
-@time genMat = nonherm(nbOne, size, diag_l, diag_u, spec, Am)
-@info "sparsity = " nnz(genMat) / (size * size)
-genspec = eigen!(Matrix(genMat)).values
+@time genMat1 = nonherm(size, diag_l, diag_u, spec1, Am1, nilp)
+@info "sparsity = " nnz(genMat1) / (size * size)
+genspec1 = eigen!(Matrix(genMat1)).values
 
-scatter(real(spec), imag(spec), markercolor = :green, markersize = 7, label="given spectrum")
-scatter!(real(genspec), imag(genspec), markercolor = :red, label="spectrum of generated matrix")
+p1=scatter(real(spec1), imag(spec1), markercolor = :green, markersize = 7, label="given spectrum")
+p1=scatter!(real(genspec1), imag(genspec1), markercolor = :red, markersize = 5, label="spectrum of generated matrix")
+
+## Non symmetric 1
+function f2(idx::Integer, size = size)
+    if idx % 2 == 0
+        return 10 * cos(((idx-1) / 2) * 2 * π / size) + 5 + 5 * sin(((idx-1) / 2) * 2 * π / size) * im
+    else
+        return 10 * cos(((idx-1) / 2) * 2 * π / size) + 5 - 5 * sin(((idx-1) / 2) * 2 * π / size) * im
+    end
+end
+
+spec2 = zeros(ComplexF64, size)
+Spectrum!(spec2, f2, size)
+
+
+Am2 = spzeros(Float64, size, size)
+initMat!(Am2, diag_l, diag_u, size; scale=0.1, sparsity=0.005)
+@time genMat2 = nonsym(size, diag_l, diag_u, spec2, Am2, nilp)
+
+@info "sparsity = " nnz(genMat2) / (size * size)
+
+genspec2 = eigen!(Matrix(genMat2)).values
+p2=scatter(real(spec2), imag(spec2), markercolor = :green, markersize = 7, label="given spectrum")
+p2=scatter!(real(genspec2), imag(genspec2), markercolor = :red, markersize = 5, label="spectrum of generated matrix")
+
+
+## Non symmetric 2
+function f3(idx::Integer, size = size)
+    return 10 * cos(((idx-1) / 2) * 2 * π / size) + 5
+end
+
+spec3 = zeros(ComplexF64, size)
+Spectrum!(spec3, f3, size)
+
+
+Am3 = spzeros(Float64, size, size)
+initMat!(Am3, diag_l, diag_u, size; scale=0.1, sparsity=0.005)
+@time genMat3 = nonsym(size, diag_l, diag_u, spec3, Am3, nilp)
+
+@info "sparsity = " nnz(genMat3) / (size * size)
+
+genspec3 = eigen!(Matrix(genMat3)).values
+p3=scatter(real(spec3), imag(spec3), ylims=(-1,1), markercolor = :green, markersize = 7, label="given spectrum")
+p3=scatter!(real(genspec3), imag(genspec3), ylims=(-1,1), markercolor = :red, markersize = 5, label="spectrum of generated matrix")
+
+p4=spy(dropzeros(genMat2), legend = :none)
+
+l = @layout [a  b;c d]
+plot(p1, p2, p3, p4, layout = l)
